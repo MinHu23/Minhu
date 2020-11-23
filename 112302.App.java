b6e3be38272a7f9f9c189f32e08a9e445d6574e0
MinHu23 <74659855+MinHu23@users.noreply.github.com>
2020-11-23 12:00:35 +0800

    Create 112302.App.java

---
+++ 112302.App.java
@@ -0,0 +1,12 @@
+	import java.util.Scanner;
+public class App {
+
+		           public void main(String[] args) throws InterruptedException {
+		               while (true){
+		            	   Thread.sleep(100);
+		                   int x = (int) (Math.random() * 100);
+		                   int y = (int) (Math.random() * 100);
+		                       System.out.println("x"+x+" "+"y"+y);
+		                   }
+		               }
+		           }
